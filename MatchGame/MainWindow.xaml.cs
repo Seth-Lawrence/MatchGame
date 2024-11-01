5937ecd9c978ec23541c92d0deb78134329f68f1
sethl <sethl@SETH>
2024-10-31 22:56:32 -0400

    Add project files.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            setUpGame();
+        }
+
+        private void setUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐿️", "🐿️",
+                "🛩️", "🛩️",
+                "📷", "📷",
+                "🕹️", "🕹️",
+                "🚂", "🚂",
+                "🥨", "🥨",
+                "🍟", "🍟",
+                "✈️", "✈️",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
